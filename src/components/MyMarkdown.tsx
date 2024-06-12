d8d90e33a0204fc59619690a4e8e82887d9bcf75
fancn21th <fancn21th@gmail.com>
2024-06-12 22:25:37 +0800

    📦 NEW: highlight

--- src/components/MyMarkdown.tsx
+++ src/components/MyMarkdown.tsx
@@ -1,17 +1,33 @@
+import { useEffect, useRef, useState } from "react";
+import Mark from "mark.js";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
 import md from "./example.md?raw";
 
 function MyMarkdown() {
+  const [keyword] = useState("财务");
+  const markdownRef = useRef(null);
+
+  useEffect(() => {
+    if (markdownRef.current) {
+      const markInstance = new Mark(markdownRef.current);
+      markInstance.unmark({
+        done: () => {
+          markInstance.mark(keyword);
+        },
+      });
+    }
+  }, [keyword]);
+
   return (
-    <>
+    <div ref={markdownRef}>
       <Markdown
         children={md}
         remarkPlugins={[remarkGfm]}
         rehypePlugins={[rehypeRaw]}
       />
-    </>
+    </div>
   );
 }
 
